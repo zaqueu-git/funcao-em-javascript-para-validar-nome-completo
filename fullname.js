24a09fc3aeb6e0d1fc3f2ac51f1ca80d693d3330
Zaqueu Alves De Oliveira Júnior <69696525+zaqueu-git@users.noreply.github.com>
2021-11-08 23:52:27 -0300

    Add files via upload

---
+++ fullname.js
@@ -0,0 +1,6 @@
+function fullName(value) {
+    if (/^[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]{3,} ([a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+){2,}$/g.test(value)) {
+        return true;
+    };
+    return false;
+}
